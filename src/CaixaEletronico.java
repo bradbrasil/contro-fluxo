270bd6d3a40f9f5ee94a61f1b036d22691035289
bradbrasil <bradbrasil13@gmail.com>
2022-09-25 18:26:52 +0100

    Estrutura de Condicional Encadaeda

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,11 +1,18 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double ValorSolicitado = 17.0;
+        double ValorSolicitado = 22.0;
 
-        if (ValorSolicitado < saldo)
+        if (ValorSolicitado < saldo){
             saldo = saldo - ValorSolicitado;
-        System.out.println(saldo);
+        System.out.println("Novo Saldo: " + saldo);
+
+
+    }
+        else
+            System.out.println("Saldo insuficiente");
+
+            System.out.println(saldo);
 
 
     }
