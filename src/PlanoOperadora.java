920dabe8121586fa8183ec3f3419fd420564c99f
bradbrasil <bradbrasil13@gmail.com>
2022-09-28 19:09:33 +0100

    controle fluxo

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,18 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+
+        switch (plano){
+            case  "T":{
+                System.out.println("5gb de Youtube");
+            }
+            case "M":{
+                System.out.println("Whats e Instagram grátis");
+            }
+            case "B":{
+                System.out.println("100min de Ligação");
+            }
+        }
+    }
+}
